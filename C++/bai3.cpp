c371f2c00c9495d09863c1ca1f18dfa1e5db0a8f
leonero2807 <leonero2807@gmail.com>
2020-03-16 22:10:09 +0700

    Hello Trung

---
+++ C++/bai3.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //chi thi tien xu ly -> su dung input, output
+using namespace std; //su dung namespace: std -> std chua thu vien iostream 
+
+int main()
+{
+    cout <<"Hello Trung" << endl;
+    return 0;
+}
+//su dung g++ voi .cpp thay vi gcc: g++ bai3.cpp -o bai3
